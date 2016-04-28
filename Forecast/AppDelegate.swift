d11ff53edcb5ba09d9b3bc76b59a9ffac4717d7b
Sean Floyd <sfloy029@uottawa.ca>
2016-04-28 19:40:42 -0400

    Initial Commit

---
+++ Forecast/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Forecast
+//
+//  Created by Sean Floyd on 2016-04-28.
+//  Copyright © 2016 Sean Floyd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
